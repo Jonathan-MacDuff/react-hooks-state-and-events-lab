9f9d9c82e4e865b1f5d20eb645d28812b934b15f
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-03-09 19:41:51 -0800

    completed lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [cartStatus, setCartStatus] = useState("")
+
+  function handleCart() {
+    if (cartStatus === "") {
+      setCartStatus("in-cart")
+    } else {
+      setCartStatus("")
+    }
+  };
+
   return (
-    <li className="">
+    <li className={cartStatus}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={() => handleCart()}>Add to Cart</button>
     </li>
   );
 }
